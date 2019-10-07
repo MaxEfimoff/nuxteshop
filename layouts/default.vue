faedfce2a186a89c4af0101b1a8ae6b26fc3b45b
Max Efimoff <emv3@ya.ru>
2019-10-07 10:14:58 +0400

    created navbar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <Navbar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Navbar from "~/components/shared/Navbar"
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 html {
