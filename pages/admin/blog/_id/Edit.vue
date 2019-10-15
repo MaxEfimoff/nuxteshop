2b3d0e29c0d86fb34f89425c3cc0594e98b0aa59
Max Efimoff <emv3@ya.ru>
2019-10-15 10:34:51 +0400

    added blog title slugs

--- pages/admin/blog/_id/Edit.vue
+++ pages/admin/blog/_id/Edit.vue
@@ -9,26 +9,26 @@
         <div class="full-page-takeover-header-button">
           <!-- TODO: Check blog validity before publishing -->
           <Modal
+            @opened="checkBlogValidity"
             openTitle="Publish"
             openBtnClass="button is-success is-medium is-inverted is-outlined"
             title="Review Details">
             <div>
               <div class="title">Once you publish blog you cannot change url to a blog.</div>
               <!-- Check for error -->
-              <div>
-                <div class="subtitle">Current Url is:</div>
+              <div v-if="!publishError">
+                <div class="subtitle">This is how your post URL will look like after publish:</div>
                 <article class="message is-success">
                   <div class="message-body">
-                    <!-- Get here actual slug -->
-                    <strong>some-slug</strong>
+                    <strong>{{ getCurrentUrl() }}/blogs/{{ slug }}</strong>
                   </div>
                 </article>
               </div>
-              <!-- <article class="message is-danger">
+              <article v-else class="message is-danger">
                 <div class="message-body">
-                  Display error here
+                  {{ publishError }}
                 </div>
-              </article> -->
+              </article>
             </div>
           </Modal>
         </div>
@@ -52,6 +52,7 @@
           @editorMounted="initBlogContent"
           @editorUpdated="updateBlogPost"
           :isSaving="isSaving"
+          ref="editor"
         />
       </div>
     </div>
@@ -61,7 +62,9 @@
 import Editor from '~/components/editor';
 import Header from '~/components/shared/Header';
 import Modal from '~/components/shared/Modal';
+
 import axios from 'axios';
+import slugify from 'slugify';
 
 import { mapState, mapActions } from 'vuex';
 
@@ -69,6 +72,12 @@ export default {
   layout: 'instructor',
   components: {
     Editor, Header, Modal
+  },
+  data() {
+    return {
+      publishError: '',
+      slug: ''
+    }
   },
   computed: {
     ...mapState('admin', ['blog', 'isSaving'])
@@ -84,12 +93,33 @@ export default {
     initBlogContent(initContent) {
       initContent(this.blog.content);
     },
+    checkBlogValidity() {
+      const title = this.$refs.editor.getNodeValueByName('title');
+      this.publishError = '',
+      this.slug = ''
+
+      if(title && title.length > 24) {
+        this.slug = this.slugify(title)
+      } else {
+        this.publishError = 'Cannot publish! Title needs to be longer than 24 characters.'
+      }
+    },
     updateBlogPost(blogData) {
       if(!this.isSaving) {
         this.$store.dispatch('admin/updateBlogPost', {data: blogData, id:this.blog._id})
         .then(console.log('Blog post updated!'))
         .catch(err => console.log(err)) 
       }
+    },
+    slugify(text) {
+      return slugify(text, {
+        replacement: '-',
+        remoove: null,
+        lower: true
+      })
+    },
+    getCurrentUrl() {
+      return process.client && window.location.origin
     }
   }
 }
